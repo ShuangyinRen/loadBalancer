619c0545b772b9e383d1abc61b826e850d3f9448
Dani <drancus92@hotmail.com>
2016-02-03 14:45:45 +0100

    Ultimos cambios

---
+++ loadBalancer/loadbalancer.h
@@ -0,0 +1,15 @@
+#ifndef OPENFLOW_LOADBALANCER_H
+#define OPENFLOW_LOADBALANCER_H
+
+#define OF_DEFAULT_SERVER_NUMBER 4
+
+// Tipo de balanceo
+enum oflb_type 
+{
+	OFLB_RANDOM,		// Aleatorio
+	OFLB_ROUND_ROBIN,	// Round Robin
+  	IP_RANDOM,         	// IP random
+	OFLB_ERROR
+};
+
+#endif
